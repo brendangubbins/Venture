7e02fb47b349cefe92e53b9e8e58c9e5bc22cabf
Harjit <harjitliyal@gmail.com>
2021-08-23 19:19:49 -0400

    fixed yelp api and got response

--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const session = require('express-session');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const cookieSession = require('cookie-session');
-const User = require('./models/User');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const cookieSession = require("cookie-session");
+const User = require("./models/User");
+require("dotenv").config();
 
 const app = express();
-const axios = require('axios');
+const axios = require("axios");
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(
   cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
     keys: [process.env.KEY],
-  }),
+  })
 );
 
 app.use(
@@ -27,7 +27,7 @@ app.use(
     secret: process.env.KEY,
     resave: true,
     saveUninitialized: true,
-  }),
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -45,68 +45,76 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-passport.use(new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
-},
-  (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleID: profile.id }).then((currentUser) => {
-      if (currentUser) {
-        done(null, currentUser);
-      } else {
-        console.log(profile);
-        new User({
-          googleID: profile.id,
-          firstName: profile.name.givenName,
-          lastName: profile.name.familyName,
-          email: profile.emails[0].value,
-          avatar: profile.photos[0].value,
-        }).save().then((newUser) => {
-          done(null, newUser);
-        }).catch((err) => console.log(err));
-      }
-    });
-  }));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleID: profile.id }).then((currentUser) => {
+        if (currentUser) {
+          done(null, currentUser);
+        } else {
+          console.log(profile);
+          new User({
+            googleID: profile.id,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            email: profile.emails[0].value,
+            avatar: profile.photos[0].value,
+          })
+            .save()
+            .then((newUser) => {
+              done(null, newUser);
+            })
+            .catch((err) => console.log(err));
+        }
+      });
+    }
+  )
+);
 
-app.get('/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email'],
-  }));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 
-app.get('/auth/google/callback',
-  passport.authenticate('google'),
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google"),
   (request, response) => {
-    response.redirect('http://localhost:3000'); // redirect after login
-  });
+    response.redirect("http://localhost:3000"); // redirect after login
+  }
+);
 
-app.get('/getUser', (request, response) => {
+app.get("/getUser", (request, response) => {
   response.send(request.user);
 });
 
-app.post('/api/search', (request, response) => {
+app.post("/api/search", (request, response) => {
   console.log("the parameters are:", request.body);
 
   var config = {
-    method: 'get',
+    method: "get",
     url: `https://api.yelp.com/v3/events?latitude=${request.body.latitude}&longitude=${request.body.longitude}&categories=${request.body.type}&start_date=${request.body.startDate}&limit=${request.body.limit}&radius=${request.body.radius}`,
     headers: {
-      'Authorization': `Bearer ${env.YELP_KEY}` 
-     }
+      Authorization: `Bearer ${process.env.YELP_KEY}`,
+    },
   };
-  
+
   axios(config)
-  .then(function (responseFromYelpAPI) {
-    console.log(JSON.stringify(responseFromYelpAPI.data));
-    return response.send(responseFromYelpAPI.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
+    .then(function (responseFromYelpAPI) {
+      console.log(JSON.stringify(responseFromYelpAPI.data));
+      return response.send(responseFromYelpAPI.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
